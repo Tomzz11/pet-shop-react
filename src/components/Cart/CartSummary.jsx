5d43e6626cff054b532b79e28824af02a85fb7a9
Thipwimon <d.leisure7@gmail.com>
2026-01-13 16:17:12 +0700

    update feature Cart add img and change code color

--- src/components/Cart/CartSummary.jsx
+++ src/components/Cart/CartSummary.jsx
@@ -1,6 +1,6 @@
 export default function CartSummary({ total, onCheckout}) {
     return  (
-        <div className="bg-gray-50 p-6 rounded-xl" >
+        <div className="bg-white p-6 rounded-xl" >
             <h3 className="text-lg font-semibold mb-4">Order Summary</h3>
 
             <div className="flex justify-between mb-2">
@@ -16,14 +16,14 @@ export default function CartSummary({ total, onCheckout}) {
 
         <div className="flex justify-between font-bold text-lg mt-4" >
             <span>Total</span>
-            <span>${total.toFixed(2)}</span>
+            <span>฿{total.toFixed(2)}</span>
         </div>
 
         <button 
             className="w-full mt-6 bg-gray-800 text-white py-2 rounded-lg hover:bg-gray-900"
             onClick={onCheckout} 
         >
-            Checkout
+            Proceed To Checkout
         </button>
     </div>
     );
