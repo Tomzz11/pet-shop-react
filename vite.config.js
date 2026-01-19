fe76da1f4b45c1947f211f2c8594b9a09750c3a9
Tommy <tomtomatoes77@gmail.com>
2026-01-19 23:52:40 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+<<<<<<< Updated upstream
 import { config } from "../front_jsd_project_group_2/src/config/config";
+=======
+import { config } from "./src/config/config.js";
+
+>>>>>>> Stashed changes
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,7 +15,11 @@ export default defineConfig({
    server: {
     proxy: {
       "/api": {
+<<<<<<< Updated upstream
         target: config.apiUrl, // 🔥 เปลี่ยนเป็น port backend ของอัส
+=======
+        target: config.apiUrl, 
+>>>>>>> Stashed changes
         changeOrigin: true,
         secure: false,
       },
